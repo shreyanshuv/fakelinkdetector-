ee8ead2c5317b12819e1a19ed051beb46e1d7e41
shreyanshuv <148850831+shreyanshuv@users.noreply.github.com>
2025-10-31 01:05:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request
 import re
 from urllib.parse import urlparse
 import tldextract
+import os
 
 app = Flask(__name__)
 
@@ -72,5 +73,10 @@ def index():
     return render_template("index.html", result=result)
 
 
+
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
+
